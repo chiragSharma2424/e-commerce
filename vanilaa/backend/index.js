c20b56aeef0449c87ac8a825a6d9a047813803a8
chirag <sharmachirag242004@gmail.com>
2025-07-27 12:12:15 +0530

    connection done

--- vanilaa/backend/index.js
+++ vanilaa/backend/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import connectToDatabase from './db/db.js';
 import userRouter from './routes/user-routes.js';
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(express.json());
+app.use(cors());
 
 app.get('/', function(req, res) {
     res.status(200).json([
