6922118c21843f6e6734e6c02ec9db06231f702f
chirag <sharmachirag242004@gmail.com>
2025-07-27 11:23:01 +0530

    token, hashing done

--- vanilaa/backend/controllers/user-controller.js
+++ vanilaa/backend/controllers/user-controller.js
@@ -1,4 +1,8 @@
 import userModel from "../models/user-model.js";
+import bcrpt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const signup = async (req, res) => {
     try {
@@ -19,15 +23,23 @@ export const signup = async (req, res) => {
             })
         }
 
+        // password must be hashed in DB
+        const hashedPassword = await bcrpt.hash(password, 10);
         const newUser = userModel.create({
             name: name,
             email: email,
-            password: password
+            password: hashedPassword
         });
+
+
+        // genrating a jwt
+        const token = jwt.sign({id: newUser._id}, process.env.JWT_SECRET, {expiresIn: '7d'});
 
         return res.status(200).json({
             success: true,
-            msg: "user signup successfully"
+            msg: "user signup successfully",
+            user: newUser,
+            token: token
         });
 
     } catch(err) {
