8fa00af51fb02fef39564e25f723fa06790de710
chirag <sharmachirag242004@gmail.com>
2025-07-27 09:13:15 +0530

    signup done

--- vanilaa/backend/controllers/user-controller.js
+++ vanilaa/backend/controllers/user-controller.js
@@ -1,8 +1,35 @@
-
+import userModel from "../models/user-model.js";
 
 export const signup = async (req, res) => {
     try {
         const { name, email, password } = req.body;
+
+        if(!name || !email || password) {
+            return res.status(400).json({
+                success: false,
+                msg: "all fields are required"
+            });
+        }
+
+        const existingUser = userModel.findOne({ email });
+        if(existingUser) {
+            return res.status(400).json({
+                success: false,
+                msg: "user already exists"
+            })
+        }
+
+        const newUser = userModel.create({
+            name: name,
+            email: email,
+            password: password
+        });
+
+        return res.status(200).json({
+            success: true,
+            msg: "user signup successfully"
+        });
+
     } catch(err) {
         console.log(`error in signup controller ${err}`);
         return res.status(500).json({
@@ -12,6 +39,9 @@ export const signup = async (req, res) => {
     }
 }
 
+
+
+// login controller 
 export const login = async (req, res) => {
     try {
         const { email } = req.body;
